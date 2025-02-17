0e8c8b53fd6e364baa3746ce0d0ed00a1834f2f5
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-02-17 09:39:53 -0300

    refazendo a navbar

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,51 +1,29 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
+import { Menu, X } from "lucide-react";
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header>
-      <nav className="border-gray-200 px-4 lg:px-6 py-2.5 bg-blue-700">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link href="/" className="flex items-center">
-            <span className="self-center text-xl text-white font-semibold whitespace-nowrap ">
-              MySite
+    <header className="h-14 lg:h-auto">
+      <nav className="border-gray-200 px-4 lg:px-6 py-2.5 bg-[#0d1b2a]">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-7xl">
+          <Link
+            href="https://www.linkedin.com/in/samuel-fava-de-brito"
+            className="flex items-center"
+          >
+            <span className="self-center text-xl text-white font-semibold whitespace-nowrap">
+              Samuel Brito
             </span>
           </Link>
 
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="lg:hidden p-2 text-gray-500 rounded-lg hover:bg-blue-900 focus:outline-none "
+            className="lg:hidden p-2 text-gray-500 rounded-lg hover:bg-blue-900 focus:outline-none"
           >
-            {isOpen ? (
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            ) : (
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            )}
+            {isOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
           </button>
 
           <div
@@ -53,29 +31,66 @@ export const Navbar = () => {
               isOpen ? "block" : "hidden"
             } lg:flex justify-between items-center w-full lg:w-auto`}
           >
-            <ul className="flex flex-col lg:flex-row lg:space-x-8 mt-4 lg:mt-0 font-medium">
-              {["Home", "Company", "Marketplace", "Features", "Team", "Contact"].map(
-                (item, index) => (
-                  <li key={index}>
-                    <Link
-                      href="/"
-                      className="block py-2 px-4 text-white rounded-lg hover:bg-blue-900 lg:hover:bg-transparent lg:p-0 "
-                    >
-                      {item}
-                    </Link>
-                  </li>
-                )
+            <ul className="flex flex-col lg:flex-row lg:space-x-8 mt-4 lg:mt-0 font-medium ">
+              <li>
+                <Link
+                  href="#home"
+                  className="block py-2 px-4 text-white rounded-lg hover:text-lg hover:text-[#778da9] hover:font-bold transition-all duration-200  lg:p-0"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#home"
+                  className="block py-2 px-4 text-white rounded-lg hover:text-lg hover:text-[#778da9] hover:font-bold transition-all duration-200 lg:p-0 "
+                >
+                  About me
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#home"
+                  className="block py-2 px-4 text-white rounded-lg hover:text-lg hover:text-[#778da9] hover:font-bold transition-all duration-200 lg:p-0 "
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#home"
+                  className="block py-2 px-4 text-white rounded-lg hover:text-lg hover:text-[#778da9] hover:font-bold transition-all duration-200 lg:p-0 "
+                >
+                  Stack
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#home"
+                  className="block py-2 px-4 text-white rounded-lg hover:text-lg hover:text-[#778da9] hover:font-bold transition-all duration-200 lg:p-0 "
+                >
+                  Contact
+                </Link>
+              </li>
+              {isOpen && (
+                <li>
+                  <Link
+                    href="/signup"
+                    className="block py-2 px-4 text-white rounded-lg hover:text-lg hover:text-[#778da9] hover:font-bold transition-all duration-200 lg:p-0 "
+                  >
+                    Eng/Pt
+                  </Link>
+                </li>
               )}
             </ul>
           </div>
 
           <div className="hidden lg:flex items-center">
-        
             <Link
               href="/signup"
               className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
             >
-              Get started
+              Eng/Pt
             </Link>
           </div>
         </div>
@@ -83,5 +98,3 @@ export const Navbar = () => {
     </header>
   );
 };
-
-
