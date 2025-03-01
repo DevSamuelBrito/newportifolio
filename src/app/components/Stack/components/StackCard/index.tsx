378ae18af31e3789de677f4e857f316c9d68bb6b
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-03-01 16:15:31 -0300

    animando a aba de stack

--- src/app/components/Stack/components/StackCard/index.tsx
+++ src/app/components/Stack/components/StackCard/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import { IconType } from "react-icons";
 
 interface StackCardProps {
@@ -10,19 +12,25 @@ interface StackCardProps {
 
 export function StackCard({ situation, technologies }: StackCardProps) {
     return (
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+        <motion.div
+            initial={{ opacity: 0, y: 50 }} 
+            whileInView={{ opacity: 1, y: 0 }} 
+            transition={{ duration: 0.8, ease: "linear" }} 
+            viewport={{once: true}}
+            className="bg-gray-800 p-6 rounded-lg shadow-lg"
+        >
             <h3 className="text-xl font-semibold text-blue-500">{situation}</h3>
             <span className="block w-24 h-1 bg-blue-500 mt-2 rounded-full"></span>
             <ul className="mt-6 text-white grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 {
                     technologies.map(({ icon: Icon, name }) => (
                         <li key={name} className="text-lg text-white pt-1 flex flex-col items-center hover:text-blue-500 transition-all duration-300">
-                            <Icon  size={48} className="sm:size-16 md:size-16" />
+                            <Icon size={48} className="sm:size-16 md:size-16" />
                             <p>{name}</p>
                         </li>
                     ))
                 }
             </ul>
-        </div>
+        </motion.div>
     );
 }
