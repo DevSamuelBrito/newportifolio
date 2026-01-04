297dbdc7a00e885247795984949e4ceec5e6f728
DevSamuelBrito <samuelbrito.dev@gmail.com>
2026-01-04 15:17:36 -0300

    feat: aplica animação  na seção de experiencia e coloca botão para visualizar mais ou menos experiencias

--- src/app/components/Experience/index.tsx
+++ src/app/components/Experience/index.tsx
@@ -1,5 +1,11 @@
 "use client"
 
+//react
+import { useState } from "react";
+
+//icons
+import { ChevronDown, ChevronUp } from "lucide-react";
+
 //components
 import ExperienceCard from "./components/ExperienceCard";
 
@@ -11,17 +17,32 @@ export function Experience() {
 
     const { t } = useTranslation();
 
+    const [showAll, setShowAll] = useState(false);
 
-    const renderData = t.experience.data.map((item, index) => (
-        <ExperienceCard
-            key={index}
-            role={item.role}
-            company={item.company}
-            period={item.period}
-            activities={item.activities}
-            technologies={item.technologies}
-        />
-    ));
+    const renderData = showAll ?
+        t.experience.data.map((item, index) => (
+            <ExperienceCard
+                key={index}
+                index={index}
+                role={item.role}
+                company={item.company}
+                period={item.period}
+                activities={item.activities}
+                technologies={item.technologies}
+            />
+        ))
+        :
+        t.experience.data.slice(0, 3).map((item, index) => (
+            <ExperienceCard
+                key={index}
+                index={index}
+                role={item.role}
+                company={item.company}
+                period={item.period}
+                activities={item.activities}
+                technologies={item.technologies}
+            />
+        ));
 
     return (
         <section
@@ -35,9 +56,26 @@ export function Experience() {
                         <span className="block w-24 h-1 bg-blue-500 mt-2 mx-auto rounded-full"></span>
                     </p>
                 </div>
-                <div className="w-full mt-4  grid grid-cols-1 gap-y-8 place-items-center ">
+                <div className="w-full mt-4 flex flex-col gap-y-8 items-center">
                     {renderData}
                 </div>
+
+                {t.experience.data.length > 3 && (
+                    <div key={t.experience.data.length} className="flex justify-center mt-10">
+                        <button
+                            onClick={() => setShowAll(!showAll)}
+                            className="px-6 py-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg transition-all duration-300 flex items-center gap-2"
+                        >
+                            {showAll ? t.experience.showLess : t.experience.showMore}
+                            {showAll ? (
+                                <ChevronUp size={20} className="text-current" />
+                            ) : (
+                                <ChevronDown size={20} className="text-current" />
+                            )}
+
+                        </button>
+                    </div>
+                )}
 
             </div>
         </section>
