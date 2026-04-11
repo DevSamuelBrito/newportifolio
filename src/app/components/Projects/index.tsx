e0414ce24584a225ef5d7c9aaad030674476173c
DevSamuelBrito <samuelbrito.dev@gmail.com>
2026-04-10 23:58:10 -0300

    feat: adiciona proteção contra vazamento de conteudo

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -53,47 +53,50 @@ export function Projects() {
     ));
 
   return (
-    <section id="projects" className="bg-gray-900 min-h-screen pt-14">
-      <div className="px-4 md:px-0 max-w-screen-xl lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-8 mx-auto min-h-fit" >
+    <div className="overflow-hidden">
+      <section id="projects" className="bg-gray-900 min-h-screen pt-14">
+        <div className="px-4 md:px-0 max-w-screen-xl lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-8 mx-auto min-h-fit" >
 
 
-        <div className="text-center">
-          <h2 className="text-3xl font-bold text-white relative inline-block">
-            {t.projects.title}
-            <span className="block w-24 h-1 bg-blue-500 mt-2 mx-auto rounded-full"></span>
-          </h2>
-        </div>
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-white relative inline-block">
+              {t.projects.title}
+              <span className="block w-24 h-1 bg-blue-500 mt-2 mx-auto rounded-full"></span>
+            </h2>
+          </div>
 
 
-        <div className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-          {displayedProjects}
-        </div>
+          <div className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+            {displayedProjects}
+          </div>
 
-        {projectsData.length > 6 && (
-          <motion.div
-            key={projectsData.length}
-            className="flex justify-center mt-10"
-            initial={{ opacity: 0, y: 30 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, ease: "easeOut" }}
-            viewport={{ once: true }}
-          >
-            <button
-              onClick={() => setShowAll(!showAll)}
-              className="px-6 py-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg transition-all duration-300 flex items-center gap-2"
+          {projectsData.length > 6 && (
+            <motion.div
+              key={projectsData.length}
+              className="flex justify-center mt-10"
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, ease: "easeOut" }}
+              viewport={{ once: true }}
             >
-              {showAll ? t.projects.showLess : t.projects.showMore}
-              {showAll ? (
-                <ChevronUp size={20} className="text-current" />
-              ) : (
-                <ChevronDown size={20} className="text-current" />
-              )}
+              <button
+                onClick={() => setShowAll(!showAll)}
+                className="px-6 py-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg transition-all duration-300 flex items-center gap-2"
+              >
+                {showAll ? t.projects.showLess : t.projects.showMore}
+                {showAll ? (
+                  <ChevronUp size={20} className="text-current" />
+                ) : (
+                  <ChevronDown size={20} className="text-current" />
+                )}
 
-            </button>
-          </motion.div>
-        )}
+              </button>
+            </motion.div>
+          )}
 
-      </div>
-    </section>
+        </div>
+      </section>
+    </div>
+
   );
 }
